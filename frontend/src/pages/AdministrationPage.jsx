f40340777e18d14e5ff488b0a08aa10315b06dac
Marketa Polakova <polakovamarketa@email.cz>
2022-11-07 15:13:40 +0100

    create routes and pages

---
+++ frontend/src/pages/AdministrationPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdministrationPage = () => {
+  return <div>AdministrationPage</div>;
+};
+
+export default AdministrationPage;
---
+++ frontend/src/pages/StudyProgrammePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudyProgrammePage = () => {
+  return <div>StudyProgrammePage</div>;
+};
+
+export default StudyProgrammePage;
