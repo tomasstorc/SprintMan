da7430ef4674a459b35ae1cb7f9a5c8f32b4c1bc
Tomas Storc <tomas.storc@gmail.com>
2022-11-18 20:49:01 +0100

    token test

--- frontend/src/pages/AdministrationPage.jsx
+++ frontend/src/pages/AdministrationPage.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import { Button } from "react-bootstrap";
 
+import { useDispatch } from "react-redux";
+import { parseToken } from "../redux/apiFetch/LoginSlice";
+
 import SubjectForm from "../components/subjectForm/SubjectForm";
 import StudyProgramForm from "../components/studyProgramForm/StudyProgramForm";
+import { useEffect } from "react";
 const AdministrationPage = () => {
+  let dispatch = useDispatch();
   const [showSubject, setShowSubject] = useState(false);
   const [showProgram, setShowProgram] = useState(false);
 
+  useEffect(() => {
+    parseToken();
+  }, [dispatch]);
   return (
     <div>
       <Button
