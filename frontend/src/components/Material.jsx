7a1d27847e33e7154a585e3a96f4df7e3c84b889
Marketa Polakova <polakovamarketa@email.cz>
2022-11-23 14:30:31 +0100

    delete a icon

--- frontend/src/components/Material.jsx
+++ frontend/src/components/Material.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import { GiBookCover } from "react-icons/gi";
 
 const Material = ({ name, link }) => {
   console.log(name);
   return (
     <a href={link}>
-      <div className="p-2 border border-dark my-1">
+      <div className="p-2 border border-dark my-1 text-dark">
         <Row>
           <Col md={"auto"}>
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3771/3771417.png"
-              alt="book"
-              style={{ width: "5rem" }}
-            />
+            <GiBookCover size={70} />
           </Col>
           <Col md={6}>
             <h5>{name}</h5>
