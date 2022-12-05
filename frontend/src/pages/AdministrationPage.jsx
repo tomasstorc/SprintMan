9dcd5bafb8c2b652ee134a605fe1c7c1dcb1461a
Marketa Polakova <polakovamarketa@email.cz>
2022-12-05 17:29:50 +0100

    role

--- frontend/src/pages/AdministrationPage.jsx
+++ frontend/src/pages/AdministrationPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Dropdown, Row, Col, Container } from "react-bootstrap";
+import { Button, Dropdown, Row, Col, Container, Alert } from "react-bootstrap";
 
 import { useSelector } from "react-redux";
 
@@ -17,104 +17,114 @@ const AdministrationPage = () => {
     subjects: "Subjects",
     users: "Users",
   };
-  const [table, setTable] = useState(type.users);
-  const { token } = useSelector((state) => state.login);
+
+  const { token, user } = useSelector((state) => state.login);
+  const [table, setTable] = useState(
+    user?.role === "admin" ? type?.users : type?.subjects
+  );
   const [showSubject, setShowSubject] = useState(false);
   const [showProgram, setShowProgram] = useState(false);
   const [showUser, setShowUser] = useState(false);
-
   return (
     <Container className="mt-5">
-      <Row>
-        <Col>
-          <Dropdown>
-            <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
-              {table}
-            </Dropdown.Toggle>
+      {user?.role === "admin" || user?.role === "editor" ? (
+        <Row>
+          <Col>
+            <Dropdown>
+              <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+                {table}
+              </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-              <Dropdown.Item
-                onClick={() => {
-                  setTable(type.users);
-                }}
-              >
-                {type.users}
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() => {
-                  setTable(type.subjects);
-                }}
-              >
-                {type.subjects}
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() => {
-                  setTable(type.programs);
-                }}
-              >
-                {type.programs}
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Col>
-
-        {table === type.users && (
-          <>
-            <Col md={"auto"}>
-              <Button
-                variant="dark"
-                onClick={() => {
-                  setShowUser(!showUser);
-                }}
-              >
-                Create User
-              </Button>
-            </Col>
-            {showUser && <UserForm show={showUser} setShow={setShowUser} />}
-            <UserTable title={type.users} token={token} />
-          </>
-        )}
-        {table === type.subjects && (
-          <>
-            <Col md={"auto"}>
-              <Button
-                variant="dark"
-                onClick={() => {
-                  setShowSubject(!showSubject);
-                }}
-              >
-                Create Subject
-              </Button>
-            </Col>
-            {showSubject && (
-              <SubjectForm show={showSubject} setShow={setShowSubject} />
-            )}
-            <SubjetsTable title={type.subjects} token={token} />
-          </>
-        )}
-        {table === type.programs && (
-          <>
-            <Col md={"auto"}>
-              <Button
-                variant="dark"
-                onClick={() => {
-                  setShowProgram(!showProgram);
-                }}
-              >
-                Create study program
-              </Button>
-            </Col>
-            {showProgram && (
-              <StudyProgramForm
-                title={type.programs}
-                show={showProgram}
-                setShow={setShowProgram}
-              />
-            )}
-            <StudyProgramTable title={type.programs} />{" "}
-          </>
-        )}
-      </Row>
+              <Dropdown.Menu>
+                {user?.role === "admin" && (
+                  <Dropdown.Item
+                    onClick={() => {
+                      setTable(type?.users);
+                    }}
+                  >
+                    {type.users}
+                  </Dropdown.Item>
+                )}
+                <Dropdown.Item
+                  onClick={() => {
+                    setTable(type?.subjects);
+                  }}
+                >
+                  {type?.subjects}
+                </Dropdown.Item>
+                <Dropdown.Item
+                  onClick={() => {
+                    setTable(type.programs);
+                  }}
+                >
+                  {type?.programs}
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+          {table === type?.users && (
+            <>
+              <Col md={"auto"}>
+                <Button
+                  variant="dark"
+                  onClick={() => {
+                    setShowUser(!showUser);
+                  }}
+                >
+                  Create User
+                </Button>
+              </Col>
+              {showUser && <UserForm show={showUser} setShow={setShowUser} />}
+              <UserTable title={type.users} token={token} />
+            </>
+          )}
+          {table === type?.subjects && (
+            <>
+              <Col md={"auto"}>
+                <Button
+                  variant="dark"
+                  onClick={() => {
+                    setShowSubject(!showSubject);
+                  }}
+                >
+                  Create Subject
+                </Button>
+              </Col>
+              {showSubject && (
+                <SubjectForm show={showSubject} setShow={setShowSubject} />
+              )}
+              <SubjetsTable title={type.subjects} token={token} />
+            </>
+          )}
+          {table === type?.programs && (
+            <>
+              <Col md={"auto"}>
+                <Button
+                  variant="dark"
+                  onClick={() => {
+                    setShowProgram(!showProgram);
+                  }}
+                >
+                  Create study program
+                </Button>
+              </Col>
+              {showProgram && (
+                <StudyProgramForm
+                  title={type.programs}
+                  show={showProgram}
+                  setShow={setShowProgram}
+                />
+              )}
+              <StudyProgramTable title={type?.programs} />{" "}
+            </>
+          )}
+        </Row>
+      ) : (
+        <Alert variant="danger">
+          <Alert.Heading>Access denied</Alert.Heading> You don't have permission
+          to visit this site
+        </Alert>
+      )}
     </Container>
   );
 };
