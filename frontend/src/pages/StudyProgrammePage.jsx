5e75b764eb224f9a0f0a9163faebd62d08f68891
Tomas Storc <tomas.storc@gmail.com>
2023-02-02 17:13:12 +0100

    mezera

--- frontend/src/pages/StudyProgrammePage.jsx
+++ frontend/src/pages/StudyProgrammePage.jsx
@@ -54,7 +54,7 @@ const StudyProgrammePage = () => {
           {programDetail?.osubjects?.map((subject) => {
             console.log(subject);
             return (
-              <Col md={"auto"}>
+              <Col md={"auto"} className="mb-4">
                 <Subject
                   title={subject.name}
                   credits={subject.credits}
@@ -67,7 +67,7 @@ const StudyProgrammePage = () => {
           {programDetail?.ossubjects?.map((subject) => {
             console.log(subject);
             return (
-              <Col md={"auto"}>
+              <Col md={"auto"} className="mb-4">
                 <Subject
                   title={subject.name}
                   credits={subject.credits}
@@ -80,7 +80,7 @@ const StudyProgrammePage = () => {
           {programDetail?.ssubjects?.map((subject) => {
             console.log(subject);
             return (
-              <Col md={"auto"}>
+              <Col md={"auto"} className="mb-4">
                 <Subject
                   key={subject._id}
                   title={subject.name}
